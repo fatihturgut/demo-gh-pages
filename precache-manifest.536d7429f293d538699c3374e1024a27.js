5b5a204d377e1c7401baf089f664c91e5c074528
Fatih Turgut <fatih.turgut@getir.com>
2020-03-10 14:39:17 +0300

    Updates

---
+++ precache-manifest.536d7429f293d538699c3374e1024a27.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e05ef72a7524d18675f71725a7474927",
+    "url": "/demo-gh-pages/index.html"
+  },
+  {
+    "revision": "87491032f81dc1286a2d",
+    "url": "/demo-gh-pages/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4327a0e87554f5d89020",
+    "url": "/demo-gh-pages/static/js/2.1a350e97.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/demo-gh-pages/static/js/2.1a350e97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87491032f81dc1286a2d",
+    "url": "/demo-gh-pages/static/js/main.6c7359b2.chunk.js"
+  },
+  {
+    "revision": "2bea02eae2d64265e224",
+    "url": "/demo-gh-pages/static/js/runtime-main.71753aa1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/demo-gh-pages/static/media/logo.5d5d9eef.svg"
+  }
+]);
